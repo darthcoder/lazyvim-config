fd3a1a5064fcab02e7339ea116405bf39d5dd443
Eric Rochester <erochest@gmail.com>
2025-09-18 07:53:35 -0400

    removed extra config for tree-sitter-just

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -1,20 +1,6 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.just = {
-        install_info = {
-          url = "https://github.com/IndianBoy42/tree-sitter-just", -- local path or git repo
-          files = { "src/parser.c", "src/scanner.c" },
-          branch = "main",
-          -- use_makefile = true -- this may be necessary on MacOS (try if you see compiler errors)
-        },
-        maintainers = { "@IndianBoy42" },
-        filetype = "just",
-      }
-    end,
+    "IndianBoy42/tree-sitter-just",
   },
 
   {
